8022c8a1e70741b0923c1dcb2b8ff28e874d9042
Apjuve <abiponce3@hotmail.com>
2022-08-05 19:03:45 -0400

    created starter format for article component

--- clima/src/components/Article.js
+++ clima/src/components/Article.js
@@ -3,7 +3,14 @@ import React from "react";
 
 const Article = () => {
     return (
-        <div>Day</div>
+        <article>
+            <h2>Date</h2>
+            <img src="" alt="" />
+            <p>Temp</p>
+            <p>Wind</p>
+            <p>Humidity</p>
+
+        </article>
     )
 }
 
