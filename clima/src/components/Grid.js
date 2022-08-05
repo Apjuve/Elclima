f3a281de621f04e0fd001a8c893d16e4134d7e94
Apjuve <abiponce3@hotmail.com>
2022-08-04 22:46:33 -0400

    setting up

---
+++ clima/src/components/Grid.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Article from "./article";
+
+
+const Grid = () => {
+    return (
+        <div className="weather-grid"><Article /></div>
+    )
+}
+
+export default Grid
+
