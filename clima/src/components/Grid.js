4300f7ee82366d89e0d2b7e4021be70111d81a1d
Apjuve <abiponce3@hotmail.com>
2022-08-06 21:54:20 -0400

    testing spacing with in grids with devtools

--- clima/src/components/Grid.js
+++ clima/src/components/Grid.js
@@ -1,10 +1,20 @@
-import React from "react";
 import Article from "./article";
+import { useState } from "react";
+
 
 
 const Grid = () => {
+
+    const [articles, setArticles] = useState ([
+        1,2,3,4,5
+    ]);
+
     return (
-        <div className="weather-grid"><Article /></div>
+        <div className="weather-grid">
+            {articles.map(article => (
+                <Article key={article} />
+            ))}
+            </div>
     )
 }
 
