c1b0a3c7acac9ebe0e6d0f60440b72e4035cb259
Apjuve <abiponce3@hotmail.com>
2022-08-10 19:34:24 -0400

    created search component

--- clima/src/App.js
+++ clima/src/App.js
@@ -1,11 +1,13 @@
-
 import './App.css';
 import Grid from './components/Grid';
+import Search from './components/Search'
+
 
 function App() {
   return (
     <div className='container'>
       <main className='main'>
+        <Search />
         <Grid />
       </main>
     </div>
