9a12d97005eedd5b2f886f3dfcacd4226a453638
Apjuve <abiponce3@hotmail.com>
2022-08-12 20:39:41 -0400

    added display flex to search

--- clima/src/components/Search.js
+++ clima/src/components/Search.js
@@ -5,12 +5,12 @@ const Search = () => {
     return (
         <div className="search">
             <form action="submit" className="search-form">
-                <label htmlFor="">
+                
                     <input type="text" className="search" id="search" placeholder="Search here" />
                     <button>
                     <RiSearchLine className="Search-icon"/>
                     </button>
-                </label>
+                
                     
             </form>
         </div>
