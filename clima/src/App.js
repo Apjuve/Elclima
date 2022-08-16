5a1f876ca07f17936cc15b9a7ad26e87009e3ba0
Apjuve <abiponce3@hotmail.com>
2022-08-15 21:38:11 -0400

    added copyrighht

--- clima/src/App.js
+++ clima/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Grid from './components/Grid';
 import Search from './components/Search'
+import Footer from './components/Footer';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <main className='main'>
         <Search />
         <Grid />
+        <Footer />
       </main>
     </div>
   );
--- clima/src/components/Footer.js
+++ clima/src/components/Footer.js
@@ -1,10 +1,16 @@
-
+import { RiGithubFill, RiLinkedinBoxFill } from 'react-icons/ri';
+import { BsFillPersonFill } from 'react-icons/bs';
 
 
 
 const Footer = () => {
     return (
-        <div className="footer">Footer</div>
+        <div className="footer">
+            <div className="links">
+            <a href="https://github.com/Apjuve"><RiGithubFill /></a>
+            <a href="https://github.com/Apjuve/Apjuve-ReactPort"><BsFillPersonFill /></a>
+            </div>
+        </div>
     )
 }
 
