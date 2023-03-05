a48cc4144f08983e3eabc7dba91401373da9ac6b
Apjuve <abiponce3@hotmail.com>
2023-03-04 23:18:21 -0500

    function reverting all grids when one is click

--- clima/src/components/Article.js
+++ clima/src/components/Article.js
@@ -1,11 +1,23 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 
-const Article = () => {
+const Article = ({ identifier }) => {
     const [active, setActive] = useState(false);
 
+    const handleNewClick = () => {
+        const filtered = [];
+        active ? setActive (false) : setActive(true);
+        const articles = document.querySelector('.weather-article');
+        const currentArticle = articles[identifier];
+        for (let i = 0; i < articles.length; i++) {
+            if (articles[i] !== currentArticle) {
+                filtered.push(articles[i]);
+            }
+        }
+    }
+
     return (
-        <article className={active ? 'weather-article active' : 'weather-article'} onClick={() => {active ? setActive(false) : setActive(true)}}>
+        <article className={active ? 'weather-article active' : 'weather-article'} onClick={() => {handleNewClick ()}}>
             <h2>Date</h2>
             <p>Temp</p>
             <p>Wind</p>
--- clima/src/components/Grid.js
+++ clima/src/components/Grid.js
@@ -7,14 +7,14 @@ import CurrentDay from "./CurrentDay";
 const Grid = () => {
 
     const articles = [
-        'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday'
+        { key: 0, day: 'monday' }, { key: 1, day: 'tuesday' }, { key: 2, day: 'wednesday' }, { key: 3, day: 'thursday' }, { key: 4, day: 'friday' }
     ];
 
     return (
         <div className="weather-grid">
             <CurrentDay />
             {articles.map(article => (
-                <Article key = {article}/>
+                <Article key = {article.day} identifier={article.key}/>
             ))}
             </div>
     )
