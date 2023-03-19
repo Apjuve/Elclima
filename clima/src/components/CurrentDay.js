d0145024380142a0481783f4d12fe2100de167e4
Apjuve <abiponce3@hotmail.com>
2023-03-19 16:46:18 -0400

    modified to mobile view

--- clima/src/components/CurrentDay.js
+++ clima/src/components/CurrentDay.js
@@ -3,11 +3,16 @@
 const CurrentDay = () => {
     return (
         <article className="currentDay">
+            <div className = "currentDay-img">
+                <img src="https://openweathermap.org/img/wn/01d@2x.png" alt="weather icon" />
+            </div>
+            <div className = "currentDay-details">
             <h1>Todays weather:</h1>
             <h3>Date</h3>
             <p>Temp</p>
             <p>Wind</p>
             <p>Humidity</p>
+            </div>
         </article>
     )
 }
